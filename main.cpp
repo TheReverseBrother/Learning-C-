13d674e9deada2aa659bb1fda40d6fa3410ea0ed
Tomas <timkanders@gmail.com>
2019-06-05 13:17:50 +0100

    Fixed Funtions
    Functions now

--- main.cpp
+++ main.cpp
@@ -25,36 +25,8 @@ int getFactorial(int number)
     return sum;
 }
 
-
-int main() {
-
-    //endl ends the line
-    cout << "Hello, World!" << endl;
-    cout << "HI";
-    cout << "HIII" << endl;
-
-
-    //Variables
-    //You cann creates constants
-    const double PI = 3.1415926535;
-
-    //Basic Variables same as java(Within Reason)
-    char myGrade = 'A';
-    bool isOn = false;
-    float Number = 3.12345;
-    double OtherNumber = 1.3214544;
-    string newString = "STRINGS";
-
-    cout << "String : " << newString <<endl;
-    cout << "Grade : " << myGrade <<endl;
-
-    int five = 5;
-
-    //Can have operaters like ++,-- results change based on if operator is before or aft6er the variable
-    five--;
-    cout << five << endl;
-
-
+void ifsAndSwitches()
+{
     //Ifs And Switches
     int age = 70;
     int ageAtlastExam = 18;
@@ -99,8 +71,40 @@ int main() {
         default:
             cout << "Hello" << endl;
     }
+}
+
+void basics()
+{
+    //endl ends the line
+    cout << "Hello, World!" << endl;
+    cout << "HI";
+    cout << "HIII" << endl;
 
 
+    //Variables
+    //You cann creates constants
+    const double PI = 3.1415926535;
+
+    //Basic Variables same as java(Within Reason)
+    char myGrade = 'A';
+    bool isOn = false;
+    float Number = 3.12345;
+    double OtherNumber = 1.3214544;
+    string newString = "STRINGS";
+
+    cout << "String : " << newString <<endl;
+    cout << "Grade : " << myGrade <<endl;
+
+    int five = 5;
+
+    //Can have operaters like ++,-- results change based on if operator is before or aft6er the variable
+    five--;
+    cout << five << endl;
+
+}
+
+void terniaryOperator()
+{
     //Terniary Operator
     /* Basic Format:
      * Variable = (condition) ? true : false;
@@ -110,6 +114,10 @@ int main() {
     int largestNum = (5>2) ? 5 : 2;
     cout << largestNum << endl;
 
+}
+
+void loops()
+{
 
     //Arrays
 
@@ -127,8 +135,6 @@ int main() {
 
     myName[0][2] = 'E';
     cout << "New Value : " << myName[0][2];
-
-
     //For Loops
     for(int i = 1; i <= 10; i++)
     {
@@ -179,7 +185,11 @@ int main() {
     }while(intNumberGuessed != 4);
 
     cout << "You Win" << endl;
+}
 
+
+void learnStrings()
+{
     //Strings
     string birthday = "Birthday";
     string happy = "Happy";
@@ -209,6 +219,10 @@ int main() {
     fullName.erase(6,7);
     cout << fullName <<endl;
 
+}
+
+void vectors()
+{
     //Vectors
     vector <int> lotteryNumbers(10);
     int lotteryNumArray[5] = {14,25,34,54,39};
@@ -223,9 +237,10 @@ int main() {
 
     //Gets Number from last can also use .front to get first
     cout << lotteryNumbers.back() << endl;
+}
 
-
-
+void functionLesson()
+{
     cout << "Functions" << endl;
     cout << addNumbers(2)<< endl;
     cout << addNumbers(2,5) << endl;
@@ -234,8 +249,10 @@ int main() {
     cout << addNumbers(2,3,4) << endl;
 
     cout << "Factorial of 3 is " << getFactorial(3) <<endl;
+}
 
-
+void fileIO()
+{
     //File I/O
     cout << endl;
     cout << "File I/O" << endl;
@@ -287,6 +304,17 @@ int main() {
         cout << endl;
     }
 
+}
+
+int main() {
+
+//    basics();
+//    terniaryOperator();
+//    loops();
+//    learnStrings();
+//    vectors();
+//    functionLesson();
+    fileIO();
 
     //Exception handling uses try catch
 
